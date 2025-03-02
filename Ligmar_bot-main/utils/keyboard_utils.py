b09a3f3d2e2c91ddb5d8c3e4fb0e8619661eaa66
SQWOZxBAB <s-awamaskal@yandex.ru>
2025-03-02 23:57:01 +0300

    Update keyboard_utils.py

--- Ligmar_bot-main/utils/keyboard_utils.py
+++ Ligmar_bot-main/utils/keyboard_utils.py
@@ -1,15 +1,16 @@
 # utils/keyboard_utils.py
 from pynput import keyboard
-from config_settings import stop_script, cmd_pressed, shift_pressed
+from config_settings import stop_script, ctrl_pressed, shift_pressed
 
 def on_press(key):
-    global stop_script, cmd_pressed, shift_pressed
+    global stop_script, ctrl_pressed, shift_pressed
 
-    if key == keyboard.Key.cmd:
-        cmd_pressed = True
+    if key == keyboard.Key.ctrl:
+        ctrl_pressed = True
     elif key == keyboard.Key.shift:
         shift_pressed = True
-    if cmd_pressed and shift_pressed:
+    if ctrl_pressed and shift_pressed:
         stop_script = True
-        print("Скрипт прерван по нажатию на cmd+shift")
+        print("Скрипт прерван по нажатию на ctrl+shift")
         return False
+        
