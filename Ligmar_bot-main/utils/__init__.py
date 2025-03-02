e61c3cd46f2be9c56529362006f562c4985393b1
SQWOZxBAB <s-awamaskal@yandex.ru>
2025-03-02 23:45:01 +0300

    Delete Ligmar_bot-main/utils directory

--- Ligmar_bot-main/utils/__init__.py
+++
@@ -1,5 +0,0 @@
-# utils/__init__.py
-from .image_utils import locate_image, find_pixel_color
-from .keyboard_utils import on_press
-from .table_utils import generate_table
-from .action_utils import cheme, is_near_resp
